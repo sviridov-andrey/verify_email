2ea936c5c21d0d1ea5e3488d1e151d97b78ea73c
Андрей <chosenfew@yandex.ru>
2023-11-20 02:08:47 +0300

    __

--- users/urls.py
+++ users/urls.py
@@ -12,19 +12,19 @@ urlpatterns = [
         'invalid_verify/',
         TemplateView.as_view(template_name='registration/invalid_verify.html'),
         name='invalid_verify'
-    ),
+    ), #3_1
 
     path(
         'verify_email/<uidb64>/<token>/',
         EmailVerify.as_view(),
         name='verify_email',
-    ),
+    ), #3
 
     path(
         'confirm_email/',
         TemplateView.as_view(template_name='registration/confirm_email.html'),
         name='confirm_email'
-    ),
+    ), #2
 
-    path('register/', Register.as_view(), name='register'),
+    path('register/', Register.as_view(), name='register'), #1
 ]
